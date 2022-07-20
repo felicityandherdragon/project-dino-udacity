cbdb656a26f433f52f269267ba1f0113b145cf64
felicityandherdragon <felicitywuny@gmail.com>
2022-07-20 17:18:17 -0400

    add dino constructor and add compare methods to the prototype

--- src/app.js
+++ src/app.js
@@ -11,55 +11,98 @@ const dinoForm = document.querySelector('form');
  * @returns raw dino data
  */
 async function fetchDinos(url) {
-    try {
-        const res = await fetch(url);
-        const { Dinos } = await res.json();
-        console.log('dinos', Dinos);
-        return Dinos;
-    } catch (err) {
-        console.log(err);
+  try {
+    const res = await fetch(url);
+    const { Dinos } = await res.json();
+    console.log('dinos', Dinos);
+    return Dinos;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+/**
+ * @description Dino object
+ * @constructor
+ * @param {Object} Object with all the raw dino data
+ */
+function dinoConstructor(dino) {
+  this.species = dino.species;
+  this.weight = dino.weight;
+  this.height = dino.height;
+  this.diet = dino.diet;
+  this.where = dino.where;
+  this.when = dino.when;
+  this.fact = dino.fact;
+}
+
+/* Compare weight method*/
+dinoConstructor.prototype.compareWeight = function (humanWeight) {
+  const ratio = (this.weight / humanWeight).toFixed(1);
+  if (ratio > 1) {
+    return `${this.species} is ${Math.floor(ratio)} times your weight`;
+  } else if (ratio < 1) {
+    return `Your weight is ${(humanWeight / this.weight).toFixed(
+      1
+    )} times the weight of ${this.species}`;
+  } else {
+    return `${this.species} is the same weight as you`;
+  }
+};
+
+/* Compare height method*/
+dinoConstructor.prototype.compareHeight = function (humanHeight) {
+  const ratio = (this.height / humanHeight).toFixed(1);
+  if (ratio > 1) {
+    return `${this.species} is ${Math.floor(ratio)} times your height`;
+  } else if (ratio < 1) {
+    return `Your height is ${(humanHeight / this.height).toFixed(
+      1
+    )} times the height of ${this.species}`;
+  } else {
+    return `${this.species} is the same height as you`;
+  }
+};
+
+/* Compare diet method*/
+dinoConstructor.prototype.compareDiet = function (humanDiet) {
+    if (this.diet === humanDiet) {
+        return `${this.species} enjoys the same diet as you!`;
+    } else {
+        return `${this.species} was ${this.diet}, and you were ${humanDiet}`;
     }
 }
 
-// Create Dino Constructor
-
-
-// Create Dino Objects
-
+/**
+ * @description create array with dino objects and human object in the right order
+ * @param {object} dinoData object with all the dino objects
+ * @param {object} humanData object with the human object
+ * @returns array with dino and human objects
+ */
+function generateSpeciesArray(dinoData, humanData) {
+    
+}
 
 /**
  * @description get human data from the form
  * @returns human object based on user input
  */
 function getHumanData(formData) {
-    const raw  = Object.fromEntries(new FormData(formData).entries());
-    const formattedHeight = Number(raw.feet) * 12 + Number(raw.inches);
-    const formattedWeight = Number(raw.weight);
+  const raw = Object.fromEntries(new FormData(formData).entries());
+  const formattedHeight = Number(raw.feet) * 12 + Number(raw.inches);
+  const formattedWeight = Number(raw.weight);
 
-    return {
-        diet: raw.diet,
-        height: formattedHeight,
-        weight: formattedWeight,
-        name: raw.name
-    }
+  return {
+    diet: raw.diet,
+    height: formattedHeight,
+    weight: formattedWeight,
+    name: raw.name,
+  };
 }
-
-
-// Create Dino Compare Method 1
-// NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-// Create Dino Compare Method 2
-// NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-// Create Dino Compare Method 3
-// NOTE: Weight in JSON file is in lbs, height in inches.
-
 
 // Generate Tiles for each Dino in Array
 function generateTiles() {
-    console.log('TBA')
+  console.log('TBA');
 }
 
 // Add tiles to DOM
@@ -69,10 +112,10 @@ function generateTiles() {
 // Event listeners
 // On form submit
 dinoForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const humanInfo = getHumanData(e.target);
-    console.log('humanInfo', humanInfo);
-    dinoForm.setAttribute('hidden', 'true')
-    const dinos = await fetchDinos('/api/dinos');
-    console.log(dinos);
-})
+  e.preventDefault();
+  const humanInfo = getHumanData(e.target);
+  console.log('humanInfo', humanInfo);
+  dinoForm.setAttribute('hidden', 'true');
+  const dinos = await fetchDinos('/api/dinos');
+  console.log(dinos);
+});
